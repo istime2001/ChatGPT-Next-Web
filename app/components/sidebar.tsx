4c56276e704e4b0fd4c7364f9a2cad383f3c1f2e
istime2001 <142368272+istime2001@users.noreply.github.com>
2023-08-16 12:38:41 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Istime's ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          欢迎来到Istime's ChatGPT公益站点，一元注册自己的ChatGPT账号请访问www.istime.top，内含教程和魔法
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
