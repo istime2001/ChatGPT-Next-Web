3ff01cfb3be9fa2af1db7eb7f9ee623f5a61d62c
istime2001 <142368272+istime2001@users.noreply.github.com>
2023-08-16 12:46:03 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           Istime's ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          welcome to Istime's ChatGPT
+          Welcome to Istime's ChatGPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
